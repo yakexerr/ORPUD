fcd62f29297dc268773538ea68bce7ac18ef76b7
yakexerr <senya.evseev.05@mail.ru>
2025-03-15 21:40:17 +0300

    new db scheme + little model

--- web/models.py
+++ web/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Task(models.Model):  # не просто класс, а именно модель
+    # primary key не добавляем (django добавит автоматически)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    PRIORITY_CHOICES = [
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+    ]
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='medium')
+    deadline = models.DateTimeField(null=True, blank=True)
+    estimated_time = models.DurationField()  # Время на выполнение
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('in_progress', 'In Progress'),
+        ('completed', 'Completed'),
+        ('on_hold', 'On Hold'),
+    ]
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
