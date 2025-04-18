9f5c630d65552a0b75300ca72b09d670f9393646
Георгий Кутасов <gera.kutasov227@gmail.com>
2025-04-18 21:00:15 +0300

    fix base pages #4

--- web/forms.py
+++ web/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth import get_user_model
 
-from web.models import TimeSlot, TimeSlotTag, Holiday
+# from web.models import TimeSlot, TimeSlotTag, Holiday
 
 User = get_user_model()
 class RegistrationForm(forms.ModelForm):
